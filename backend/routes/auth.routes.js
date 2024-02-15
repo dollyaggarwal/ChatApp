0353b11ddef48b3143639eae58e257b6cd2f77db
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-15 15:52:59 +0530

    Created user modeel and created mongodb connection file.

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,9 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.get("/signup", (req,res)=>{
-    res.send("Login Route");
-});
-router.get("/login", (req,res)=>{
-    res.send("Login Route");
-});
-router.get("/logout", (req,res)=>{
-    res.send("Login Route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
