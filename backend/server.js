c2100e7825ae36053893311c0d5b3a9993aa72b8
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-15 22:41:28 +0530

    completed get users functionality in user controllers and user routes.

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json());// to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes);
+app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
