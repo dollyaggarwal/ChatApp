58f31d0d3ff271ef5efa5b390c7f6fbf3db6469a
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-16 19:35:52 +0530

    creating design for login page and signup page.

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+	return;
+	<div className='flex flex-col items-center min-w-96 mx-auto'>
+		<div
+			className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding 
+      backdrop-filter backdrop-blur-lg 
+      bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>Sign Up 
+      <span className='text-yellow-500'> ChatApp</span>
+      </h1>
+      </div>
+	</div>;
+};
 
-export default SignUp
+export default SignUp;
