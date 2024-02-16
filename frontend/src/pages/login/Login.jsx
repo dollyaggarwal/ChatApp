5718aeb9f017e4b5c60dd034597e846c9d2ebb21
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-16 23:37:35 +0530

    Designing sidebar with Conversations list

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Login = () => {
   return (
     <div className='flex flex-col items-center min-w-96 mx-auto'>
@@ -33,4 +31,41 @@ const Login = () => {
     </div>
   );
 };
-export default Login
+export default Login;
+
+//STARTER CODE FOR LOGIN
+
+// const Login = () => {
+//   return (
+//     <div className='flex flex-col items-center min-w-96 mx-auto'>
+//       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg 
+//       bg-opacity-0'>
+//       <h1 className='text-3xl font-semibold text-center text-gray-300'>Login 
+//       <span className='text-yellow-500'> ChatApp</span>
+//       </h1>
+
+//       <form>
+//         <div>
+//           <label className='label p-2'>
+//             <span className='text-base label-text'>Username</span>
+//           </label>
+//           <input type='text' placeholder='Enter Username' className='w-full input input-bordered h-10'/>
+//         </div>
+//         <div>
+//         <label className='label'>
+//             <span className='text-base label-text'>Password</span>
+//             </label>
+//             <input type='password' placeholder='Enter password' className='w-full input input-bordered h-10'/>
+//         </div>
+//         <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+//           {"Don't"} have an account?
+//         </a>
+//         <div>
+//           <button className='btn btn-block btn-sm mt-2 broder border-slate-700'>Login</button>
+//         </div>
+//       </form>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Login;
