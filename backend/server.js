4844c0ed659def68d523e37edd95c93d27e1e2e9
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-18 18:58:37 +0530

    "finished"

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -8,9 +9,11 @@ import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
+import exp from 'constants';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());// to parse the incoming requests with JSON payloads (from req.body)
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.use("*", (req,res)=>{
+	res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT, () => {
 	connectToMongoDB();
