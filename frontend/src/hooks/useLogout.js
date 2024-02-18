94f6538f6c1bc50aba2e8313edc2bdacaa610d98
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-02-18 11:02:04 +0530

    completed Login functionality with UI linking.

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
 const useLogout = () => {
   const [loading, setLoading] = useState(false);
   const {setAuthUser} = useAuthContext();
